0acae16ac61b2b0860b1ff90bb1aae4966e94837
Ryan Gallo <ryan.j.gallo@gmail.com>
2020-10-14 16:50:18 -0400

    Initial commit for project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const [text, setText] = useState("let's go mills baby love da mills");
+    
+    const getInitialism = () => {
+        return text.replace(/\n/g, " ").split(" ").map(word => word.match(/(^[\d\./]+\w?%?)|(^[\w])/g)).join("").toUpperCase();
+    };
+    
+    return (
+        <div>
+            <textarea value={text} onChange={e => setText(e.target.value)} />
+            <h1>{getInitialism()}</h1>
+        </div>
+        
+    );
 }
 
 export default App;
