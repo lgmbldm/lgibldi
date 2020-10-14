6d88bcb278bfa8987bb039d357d05799178d3de6
Ryan Gallo <ryan.j.gallo@gmail.com>
2020-10-14 16:35:45 -0400

    Updates

---
+++ precache-manifest.f4943323d5d6258a211799dda0a953af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581efadbc669b3f932e27af6bd355879",
+    "url": "/lgibldi/index.html"
+  },
+  {
+    "revision": "74f035b34c4e7bca8936",
+    "url": "/lgibldi/static/css/main.26a4d63e.chunk.css"
+  },
+  {
+    "revision": "99ed369291d8c5d7e3fa",
+    "url": "/lgibldi/static/js/2.f709c5c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lgibldi/static/js/2.f709c5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f035b34c4e7bca8936",
+    "url": "/lgibldi/static/js/main.73760b57.chunk.js"
+  },
+  {
+    "revision": "c90cbcf59c774ed9cdaa",
+    "url": "/lgibldi/static/js/runtime-main.a4c46130.js"
+  }
+]);
